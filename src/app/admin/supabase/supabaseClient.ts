47af35933beeccd6becc14d5c5f6e2e2ce68ccba
65022971_re-Score <119997906+iouios@users.noreply.github.com>
2025-04-10 14:48:58 +0700

    update env

--- src/app/admin/supabase/supabaseClient.ts
+++ src/app/admin/supabase/supabaseClient.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://tysasgfmndcgptbtmzgq.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5c2FzZ2ZtbmRjZ3B0YnRtemdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQyNTgzNDQsImV4cCI6MjA1OTgzNDM0NH0.-4z3xVY-Ljua-mRhmzezzEzkKdcJpniCgu91tlFwIC8'; 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
